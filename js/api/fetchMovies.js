88acf873fbbcf89772e7f447b64b324470cbbab7
siljemogster <125352923+siljemogster@users.noreply.github.com>
2023-12-26 15:50:56 +0100

    fetch

--- js/api/fetchMovies.js
+++ js/api/fetchMovies.js
@@ -1,15 +1,2 @@
-async function fetchMovies () {
-    try {
-        const response = await fetch(url);
-    const json = await response.json();
-    displayMovies(json);
-    }
-    catch(error) {
-        const movieContainer = document.querySelector("#movies-container");
-        movieContainer.innerHTML = '<div class="error">There was an error fetching the movies</div>';
-        console.error(error);
-    }      
-}
-
-fetchMovies();
+ 
 
