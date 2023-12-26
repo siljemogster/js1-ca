c6310d407a9552f7269d895b245b3e2fab815d83
siljemogster <125352923+siljemogster@users.noreply.github.com>
2023-12-26 09:58:50 +0100

    improvements

--- js/movie.js
+++ js/movie.js
@@ -1,38 +1,31 @@
 const queryString = document.location.search;
-const params = new URLSearchParams(queryString);  
+const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
 if (!id) {
-document.location.href = "/"; 
-
+  document.location.href = "/";
 }
-
 
 const url = `https://api.noroff.dev/api/v1/square-eyes/${id}`;
 
 async function getMovies() {
+  try {
+    const response = await fetch(url);
+    const details = await response.json();
 
-try {
-
-    const response = await fetch(url); 
-    const details = await response.json(); 
-
-    const container = document.querySelector("#movie-details"); 
+    const container = document.querySelector("#movie-details");
 
     container.innerHTML = `<div class="movie-card">
                                <h2>${details.title}</h2>
                                <img src="${details.image}" alt="${details.title}" />
                                              </div>`;
 
-    console.log(details); 
-
-
-} catch (error) {
-    console.log(error); 
+    console.log(details);
+  } catch (error) {
+    console.log(error);
     const container = document.querySelector("#movie-details");
     container.innerHTML = `<div class="error">An error occured when calling the movies APi</div>`;
-
-}   
+  }
 }
 
-getMovies(); 
+getMovies();
