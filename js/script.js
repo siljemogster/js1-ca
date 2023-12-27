48afb909a0cea42fd89c1cfa1b1f48404e4f76b9
siljemogster <125352923+siljemogster@users.noreply.github.com>
2023-12-27 12:49:30 +0100

    removed commented out

--- js/script.js
+++ js/script.js
@@ -33,18 +33,6 @@ function displayMovies(movies) {
 
         const movieDetails = createMovie(movie)
         movieContainer.append(movieDetails); 
-
-
-
-
-       // movieContainer.innerHTML += `<div class="movie">
-         //                                <h3>${movie.title}</h3>
-           //                              <p>${movie.description}</p>
-             //                            <p>${movie.genre}</p>
-               //                          <p>${movie.released}</p>
-                 //                        <p>${movie.rating}</p>
-                   //                      <p>$${movie.price}</p>             
-                     //                  </div>`;
     });
 
 }
@@ -81,56 +69,3 @@ function createMovie(movie) {
 
 
 
-
-
-
-/* const url = "https://api.noroff.dev/api/v1/square-eyes";
-
-async function getMovies() {
-
-
-try {
-
-    const response = await fetch(url);
-    const movies = await response.json();
-    //console.log(movies); 
-
-    const resultsContainer = document.querySelector("#container");
-
-    resultsContainer.innerHTML =""; 
-
-    movies.forEach(function (movie) {
-    /* console.log(movie); 
-
-        resultsContainer.innerHTML += `<div class="card">
-                                          <h3>${movie.title}</h3>
-                                          <p>Description: ${movie.description}</p>
-                                          <p>Genre: ${movie.genre}</p>
-                                          <p>Released: ${movie.released}</p>
-                                          <p>Price: ${movie.price}</p>
-                                          <a href="movie.html?id=${movie.id}">View details</a>
-                                       </div>`; 
-    const div = document.createElement("div");  
-    div.classList.add("product")    
-    
-    const heading = document.createElement("h3");
-    heading.innerText =product.title; 
-
-    div.append(heading);
-
-    productContainer.append(div);
-
-
-
-});
-}
-
-
-catch (error) {
-    console.log(error);
-    const resultsContainer = document.querySelector("#container");
-    resultsContainer.innerHTML ='<div class="error">An error occured when calling the movies API';
-
-}
-}
-getMovies(); */ 
