22862619435a464613da6523aa0b2623110c6cba
siljemogster <125352923+siljemogster@users.noreply.github.com>
2023-12-27 12:31:40 +0100

    movie details page

--- js/movie.js
+++ js/movie.js
@@ -11,7 +11,7 @@ async function fetchMovie(id) {
  
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(detailUrl);
     const result = await response.json();  
     displayMovie(result); 
   }
@@ -24,25 +24,53 @@ async function fetchMovie(id) {
 }
 
 
+
 fetchMovie(id);
 
 
 function displayMovie(movie) {
-  console.log(movie); 
+    document.title = movie.title; 
+
   const container = document.querySelector("#movie-container"); 
 
-  console.log(container); 
+  container.innerHTML = "";
 
-
+  const loader = document.querySelector('.loader');
+  if (loader) {
+  loader.remove(); 
+}
 
   const heading = document.createElement("h1"); 
   heading.innerText = movie.title;
- 
+
+  const image = document.createElement("img");
+  image.src = movie.image; 
+  image.alt = movie.title; 
+
   const description = document.createElement("p"); 
   description.innerText = movie.description; 
 
+  const genre = document.createElement("p"); 
+  genre.innerText = `Genre: ${movie.genre}`;
+
+  const rating = document.createElement("p"); 
+  rating.innerText = `Rating: ${movie.rating}`;
+
+  const released = document.createElement("p"); 
+  released.innerText = `Year: ${movie.released}`;
+
+  const price = document.createElement("p"); 
+  price.innerText = `Price: $${movie.price}`;
+
+  
+
   container.append(heading);
+  container.append(image); 
   container.append(description); 
+  container.append(genre);
+  container.append(rating);
+  container.append(released);
+  container.append(price);
 
 }
 
